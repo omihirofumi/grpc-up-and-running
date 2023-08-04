7bcd0b7c22e6ca1df25213f543e69f88097adaa6
hirofumi omi <omihirofumi@gmail.com>
2023-08-04 19:43:42 +0900

    add docker

---
+++ client-go/product/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	pb "product/proto/v1"
+)
+
+func main() {
+
+	args := os.Args
+	var hostname, port string
+	if len(args) < 3 {
+		hostname = "localhost"
+		port = "50051"
+	} else {
+		hostname = args[1]
+		port = args[2]
+	}
+
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", hostname, port), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect grpc server: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewProductInfoClient(conn)
+	id, err := client.AddProduct(context.Background(), &pb.Product{Name: "apple", Price: 100})
+	if err != nil {
+		log.Println("failed to add product")
+	}
+	log.Println(id)
+}
